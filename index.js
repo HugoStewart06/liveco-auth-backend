c5357044974f1ae33df587ed1dbc6d155b641999
Benoît Hubert <benoithubert@gmail.com>
2021-01-26 12:00:53 +0100

    Add cookie-parser and route to check JWT in cookie

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 const { port } = require('./config');
 const authRouter = require('./routes/auth');
 
 const app = express();
 app.use(morgan('dev'));
+// Middleware permettant de "parser" (analyser) les cookies
+// reçus dans les headers de requête. Va renseigner res.cookies,
+// objet contenant les cookies sous forme de paires clé-valeur
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+// CORS = Cross Origin Request Sharing
+// https://developer.mozilla.org/fr/docs/Web/HTTP/CORS
+// Permet d'accepter des requêtes provenant d'une origine
+// (ici http://localhost:3000) différente de celle du backend
+// (ici http://localhost:5000).
+// Ici on configure les CORS pour n'autoriser que les requêtes
+// provenant du front, et pour autoriser les credentials (identifiants,
+// en l'occurence ici le token transmis via un cookie)
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }),
+);
 app.use(authRouter);
 
 app.get('/', (req, res) => res.send('Express server is up and running!'));
--- routes/auth.js
+++ routes/auth.js
@@ -133,4 +133,25 @@ router.post('/api/auth/login', checkRequiredAuthFields, (req, res) => {
   });
 });
 
+router.get('/api/auth/check', (req, res) => {
+  // On récupère le token (JWT) depuis les cookies
+  const { token } = req.cookies;
+  // Si le token n'est pas présent => erreur 401
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  // Vérifie la validité du JWT : il ne doit pas avoir expiré,
+  // et doit avoir été généré avec notre clé secrète privateKey
+  return jwt.verify(token, privateKey, (err, payload) => {
+    // Si erreur (JWT expiré, signature invalide) => 401
+    if (err) {
+      console.error(err);
+      return res.sendStatus(401);
+    }
+    // Pas d'erreur : le JWT est valide. On renvoie son contenu
+    // (sa payload ou charge utile, contenant ici l'id de l'utilisateur)
+    return res.json(payload);
+  });
+});
+
 module.exports = router;
